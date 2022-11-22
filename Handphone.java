3f059058d3447f0cccc902ef33389c929089b6ec
Ichasyahputri <118749731+Ichasyahputri@users.noreply.github.com>
2022-11-22 14:49:21 +0700

    Add files via upload

---
+++ Handphone.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.handphone;
+
+/**
+ *
+ * @author ICHA SYAHPUTRI
+ */
+public class Handphone {
+
+    public static void main(String[] args) {
+        String merk;
+        String so;
+        String ram;
+        String rom;
+        
+        merk ="oppo";
+        so="Android";
+        ram="16 GB";
+        rom="128 GB";
+        
+        System.out.println("Spesifikasi My Handpohe");
+        System.out.println("merk :"+merk);
+        System.out.println("Sistem Operasi :"+so);
+        System.out.println("Ram :"+ram);
+        System.out.println("Rom :"+rom);
+    }
+}
